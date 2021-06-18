b00216539959ba9609dc5b4d71a526b652193cdc
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 23:41:11 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -51,3 +51,21 @@ int factors(int n, int a[]) {
   }
   return j;
 }
+int max(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (max(x, 7) == 10);
+
+  int y[] = {5};
+  assert (max(y, 1) == 5);
+  printf("Max: passed\n");
+  }
+int max(int a[], int n) {
+  int i, x = a[0];
+  for(i = 0; i<n; i++) {
+    if(x<a[i])
+      x = a[i];
+  }
+  return x;
+}
