fa0cf393a56fc60715f0f31ef6693fa958ce1f6e
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 23:58:04 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,16 +1,7 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
 #include <assert.h>
-float average(int [], int);
 
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert ((average(x, 7) - 4.7142) < 0.001);
-
-  int y[] = {5};
-  assert (average(y, 1) == 5.0);
-  printf("Average: passed\n");
-  }
 float average(int a[], int n) {
   int i; float sum = 0;
   for(i = 0; i<n; i++) {
@@ -19,27 +10,7 @@ float average(int a[], int n) {
   sum = sum/n;
   return sum;
 }
-#include <stdio.h>
-#include <assert.h>
-int factors(int, int []);
 
-int main(void) {
-  int ret[100] = {0};
-  int count = factors(180, ret);
-  assert (count == 5);
-  assert (ret[0] == 2);
-  assert (ret[1] == 2);
-  assert (ret[2] == 3);
-  assert (ret[3] == 3);
-  assert (ret[4] == 5);
-
-
-  count = factors(143, ret);
-  assert (count == 2);
-  assert (ret[0] == 11);
-  assert (ret[1] == 13);
-  printf("Factors: passed\n");
-}
 int factors(int n, int a[]) {
   int i, j = 0;
   int x = n;
@@ -54,18 +25,6 @@ int factors(int n, int a[]) {
   }
   return j;
 }
-#include <stdio.h>
-#include <assert.h>
-int max(int [], int);
-
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert (max(x, 7) == 10);
-
-  int y[] = {5};
-  assert (max(y, 1) == 5);
-  printf("Max: passed\n");
-  }
 int max(int a[], int n) {
   int i, x = a[0];
   for(i = 0; i<n; i++) {
@@ -74,18 +33,6 @@ int max(int a[], int n) {
   }
   return x;
 }
-#include <stdio.h>
-#include <assert.h>
-int min(int [], int);
-
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert (min(x, 7) == -3);
-
-  int y[] = {5};
-  assert (min(y, 1) == 5);
-  printf("Min: passed\n");
-  }
 int min(int a[], int n) {
   int i, x = a[0];
   for(i = 0; i<n; i++) {
@@ -94,18 +41,6 @@ int min(int a[], int n) {
   }
   return x;
 }
-#include <stdio.h>
-#include <assert.h>
-int mode(int [], int);
-
-int main(void) {
-  int x[] = {4, 9,5,6,5,10,0,2,-3, -3,4, 4};
-  assert ((mode(x, 12) == 4));
-
-  int y[] = {5};
-  assert (mode(y, 1) == 5);
-  printf("Mode: passed\n");
-  }
 int mode(int a[], int n) {
   int i, j;
   int maxcount = 0, maxvalue = 0;
