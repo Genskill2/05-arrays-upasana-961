b779166a37616ce0b82981ea92e8ccb17557cd46
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 23:18:11 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,2 +1,10 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+float average(int a[], int n) {
+  int i; float sum = 0;
+  for(i = 0; i<n; i++) {
+    sum+=a[i];
+  }
+  sum = sum/n;
+  return sum;
+}
