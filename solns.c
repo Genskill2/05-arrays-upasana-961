2aad4a0c60a5b77c3cc9e4e1822152b190012d9a
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 23:41:47 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -69,3 +69,21 @@ int max(int a[], int n) {
   }
   return x;
 }
+int min(int [], int);
+
+int main(void) {
+  int x[] = {9,5,6,10,2,-3,4};
+  assert (min(x, 7) == -3);
+
+  int y[] = {5};
+  assert (min(y, 1) == 5);
+  printf("Min: passed\n");
+  }
+int min(int a[], int n) {
+  int i, x = a[0];
+  for(i = 0; i<n; i++) {
+    if(x>a[i])
+      x = a[i];
+  }
+  return x;
+}
