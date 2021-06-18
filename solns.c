c25fa5a5cab80c07069142e32b12aadbc8f6ee76
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-19 00:00:21 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -2,58 +2,69 @@
 #include <stdio.h>
 #include <assert.h>
 
-float average(int a[], int n) {
-  int i; float sum = 0;
-  for(i = 0; i<n; i++) {
-    sum+=a[i];
+int max(int a[], int n){
+  
+  int i,x=a[0];
+  for(i=0;i<n;i++)
+  {
+      if(x<a[i])
+       x=a[i];
   }
-  sum = sum/n;
+   return x; 
+}
+int min(int a[], int n){
+  int i,x=a[0];
+  for(i=0;i<n;i++)
+  {
+      if(x>a[i])
+       x=a[i];
+    }
+   return x;  
+}
+float average(int a[], int n){ 
+   int i ; float sum=0;
+   for(i=0;i<n;i++)
+  {
+    sum+=a[i]; 
+  }
+  sum=sum/n;
   return sum;
-}
-
-int factors(int n, int a[]) {
-  int i, j = 0;
-  int x = n;
-  for(i = 2; i<=n; i++) {
-    if (n%i==0) {
-      a[j] = i;
-      printf("%d %d %d", j, n, a[j]);
-      n = n/i;
-      i = 1;
-      j++;
-    }
   }
-  return j;
-}
-int max(int a[], int n) {
-  int i, x = a[0];
-  for(i = 0; i<n; i++) {
-    if(x<a[i])
-      x = a[i];
-  }
-  return x;
-}
-int min(int a[], int n) {
-  int i, x = a[0];
-  for(i = 0; i<n; i++) {
-    if(x>a[i])
-      x = a[i];
-  }
-  return x;
-}
-int mode(int a[], int n) {
-  int i, j;
-  int maxcount = 0, maxvalue = 0;
-  for(i=0; i<n, ++i) {
+int mode(int a[] , int n){
+  int i,j ;
+  int maxcount=0 , maxvalue=0;
+  for(i=0 ; i<n; ++i){
     int count = 0;
-    for(j = 0; j<n; ++j) {
-      if(a[i] == a[j])
-        ++count;
+    for(j=0; j<n ; ++j){
+      if(a[i]==a[j])
+        ++count ;
     }
-    if(count>maxcount) {
+    
+    if(count > maxcount){
       maxcount = count;
       maxvalue = a[i];
     }
   }
-    return maxvalue;
+   return maxvalue;
+ }
+int factors(int n, int a[]){
+  
+  int i, j=0;
+  int x=n;
+  for(i=2;i<=n;i++)
+  {
+    
+    if(n%i==0)
+    {
+      
+      a[j]=i;
+     // printf("%d %d %d\n",j,n,a[j]);
+      n=n/i;
+      
+      i=1;
+      j++;
+      
+    } 
   }
+  return j;  
+}
