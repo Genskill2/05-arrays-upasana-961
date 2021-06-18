4adbacb4b568b5602ea134cfff9c6971076c6864
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 23:20:31 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -38,3 +38,19 @@ int min(int a[], int n) {
   }
   return x;
 }
+int mode(int a[], int n) {
+  int i, j;
+  int maxcount = 0, maxvalue = 0;
+  for(i=0; i<n, ++i) {
+    int count = 0;
+    for(j = 0; j<n; ++j) {
+      if(a[i] == a[j])
+        ++count;
+    }
+    if(count>maxcount) {
+      maxcount = count;
+      maxvalue = a[i];
+    }
+  }
+    return maxvalue;
+  }
