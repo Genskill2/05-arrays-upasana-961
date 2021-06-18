538e260acdf34c76ba335aa9c62076f4074d7fa4
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 22:57:00 +0400

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -12,3 +12,11 @@ int main(void) {
   assert (average(y, 1) == 5.0);
   printf("Average: passed\n");
   }
+float average(int a[], int n) {
+  int i; float sum = 0;
+  for(i = 0; i<n; i++) {
+    sum+=a[i];
+  }
+  sum = sum/n;
+  return sum;
+}
