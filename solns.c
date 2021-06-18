9712229b5f18c71a25a9390ec5e68de9aff558db
upasana-961 <84665075+upasana-961@users.noreply.github.com>
2021-06-18 23:40:35 +0400

    Update solns.c

--- solns.c
+++ solns.c
@@ -18,3 +18,36 @@ float average(int a[], int n) {
   sum = sum/n;
   return sum;
 }
+int factors(int, int []);
+
+int main(void) {
+  int ret[100] = {0};
+  int count = factors(180, ret);
+  assert (count == 5);
+  assert (ret[0] == 2);
+  assert (ret[1] == 2);
+  assert (ret[2] == 3);
+  assert (ret[3] == 3);
+  assert (ret[4] == 5);
+
+
+  count = factors(143, ret);
+  assert (count == 2);
+  assert (ret[0] == 11);
+  assert (ret[1] == 13);
+  printf("Factors: passed\n");
+}
+int factors(int n, int a[]) {
+  int i, j = 0;
+  int x = n;
+  for(i = 2; i<=n; i++) {
+    if (n%i==0) {
+      a[j] = i;
+      printf("%d %d %d", j, n, a[j]);
+      n = n/i;
+      i = 1;
+      j++;
+    }
+  }
+  return j;
+}
